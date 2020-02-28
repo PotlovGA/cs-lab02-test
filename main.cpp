0724d98f2c3d0148ac6d0e3516c99ff0142846c5
Alice(PotlovGA) <alice@example.com>
2020-02-28 20:40:59 +0400

    code: изменение формата записи

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "a + b = " << a+b;
-    cout << "a - b = " << a-b;
+    cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n';
     return 0;
 }
